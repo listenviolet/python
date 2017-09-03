0b3a59e7ad7b2f1ff7bac857d3f9008406539a72
SHIYU LIU <syu@localhost.localdomain>
2017-09-03 03:01:12 +0200

    input and output

---
+++ 1-3-inout.py
@@ -0,0 +1,11 @@
+print('The quick brown fox','jumps over','the lazy dog')
+
+print('100+300=',100+300)
+
+name=input()
+print(name)
+
+name=input('please input your name: ')
+print('hello,',name)
+
+print('1024*768=',1024*768)
