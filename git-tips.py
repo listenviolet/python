71e02f84aa42b64cfcf8fec457aa96aedbb43276
SHIYU LIU <syu@localhost.localdomain>
2017-09-03 05:19:07 +0200

    git-tips

--- git-tips.py
+++ git-tips.py
@@ -25,3 +25,13 @@ git commit -m "remove test.txt"
 #git checkout 用版本库里的版本替换工作区的版本
 git checkout -- test.txt
 
+#要关联一个远程库，使用命令
+git remote add origin git@server-name:path/repo-name.git
+
+#关联后，使用命令
+git push -u origin master
+#第一次推送master分支的所有内容；
+
+#此后，每次本地提交后，只要有必要，就可以使用命令
+git push origin master
+#推送最新修改；
